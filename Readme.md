f6151e967704aa6682520e124bc7280d585e5c5f
Christopher Grey <chrisgrey09@gmail.com>
2013-08-07 01:23:12 -0400

    Added readme.

---
+++ Readme.md
@@ -0,0 +1,77 @@
+
+getConfig
+
+{ value:null }
+Initiates a request to get the current droid control configuration. Droid will return a “configuration” event. There is no data value. 
+
+
+drive
+
+{
+     “direction”:int,
+      “velocity”: int
+}
+
+Direction and Velocity are each values between -255 and +255. These values will be translated by the droid into Left/Right foot motor pulse values. 
+
+rotateDome
+
+
+{
+     “value”:int
+}
+
+Rotates the dome. Value of -255 to +255 will change direction and speed. 
+
+changeDisplay
+Changes various display modes on the droid. 
+
+{
+     “display”:displayName,
+     “mode”: modename,
+     “displayText”: text
+}
+
+displayName = “fld1”,”fld2”,”rld” or “all” etc. 
+modeName = “random” or some other pre-defined name. 
+displayText = Text to display if mode supports it. 
+
+
+playSound
+Plays a sound or sound playlist on the droid. 
+
+{
+     “type”:soundType,
+     “name”:soundName
+}
+
+type: either ‘single’ or ‘playlist’
+name: the name of the file or playlist
+
+
+changeLegMode
+
+Changes the leg orientation of the droid for those that support a 2-3-2 configuration. 
+
+{
+     “mode”:modeName
+}
+
+modeName = “2leg” or “3leg”
+
+
+changePanels
+Changes open/closed state of dome panels. 
+{
+     “pp1”:panelState,
+     “pp4”: panelState
+}
+
+panelState = int of 0 to 100 that indicates full closed or full open. 
+
+
+custom
+Available to send custom commands for any unimplemented events. Expansion for droidbuilder to use. 
+{
+     ‘data’:customData
+}
--- app.js
+++ app.js
@@ -4,32 +4,72 @@
  */
 
 var express = require('express')
+  , app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , path = require('path')
   , routes = require('./routes')
   , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+  , io = require('socket.io').listen(server)
 
-var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+var clients = {};
 
-// development only
-if ('development' == app.get('env')) {
+
+server.listen(7777)  ;
+console.log('Server running at http://127.0.0.1:7777/');
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 7777);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
   app.use(express.errorHandler());
-}
+});
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/users', user.list); 
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+setInterval(function() {
+		console.log('[Sending PING.]');
+		io.sockets.emit('ping',{text:'PING'});
+			
+	},6000); 
+
+
+
+
+io.sockets.on('connection',function(socket) {
+	console.log('Connection Received. Socket ID: ' + socket.id);
+	clients[socket.id] = socket;
+	
+	socket.on('pong',function(data) {
+		console.log('[Received PONG.]');
+	});
+	socket.on('subscribe', function(roomNumber) { 
+		
+		socket.join(roomNumber);
+		console.log('Socket joined room: ' + roomNumber) 
+		//sendConfiguration(socket,roomNumber);
+	});
+	socket.on('unsubscribe', function(roomNumber) { 
+		socket.leave(roomNumber);
+		console.log('Socket left room: ' + roomNumber) 
+		//sendERROR(socket,"Something went very very wrong!");
+	});
+	socket.on('configuration', function(data) { 
+		// Make sure the client is connected to a room. If not, send error.
+		console.log('----Config--Config--Config--Config----');
+
+		//console.log('Socket requested configuration. Will send this data: ' + JSON.stringify(roomData[data]) + ' For room ' + data);
+		//sendConfiguration(socket,data);
+	});
 });
